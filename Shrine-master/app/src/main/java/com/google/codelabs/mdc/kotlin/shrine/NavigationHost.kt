b739056dda9ec48369f8de1042e214465a1e194b
kemerekehe <2210817220029@mhs.ulm.ac.id>
2024-06-28 11:58:15 +0800

    first commit modul 5

---
+++ Shrine-master/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/NavigationHost.kt
@@ -0,0 +1,9 @@
+package com.google.codelabs.mdc.kotlin.shrine
+
+import androidx.fragment.app.Fragment
+
+
+
+interface NavigationHost {
+    fun navigateTo(fragment: Fragment, addToBackstack: Boolean)
+}
